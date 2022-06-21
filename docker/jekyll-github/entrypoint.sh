0e2955784f2d0416bbd16ab6695eb1cac6d7d372
Enno Boland <g@s01.de>
2022-06-21 09:58:00 +0200

    jekyll-github: do not use bundle exec

--- docker/jekyll-github/entrypoint.sh
+++ docker/jekyll-github/entrypoint.sh
@@ -3,9 +3,9 @@
 bundle install
 
 if [ $# -eq 0 ]; then
-	exec bundle exec jekyll serve
+	exec jekyll serve
 elif [ -z "${1##/*}" ]; then
 	exec "$@"
 else
-	exec bundle exec jekyll "$@"
+	exec jekyll "$@"
 fi
